8d3b2b3f9fe359eb3b5d5e04f30c392dbc6840e2
Nestiftechnologies2021 <taijasikaveri@gmail.com>
2025-05-13 11:27:31 +0530

    Added Internships&Training

---
+++ my-app/src/Components/Internship&Training/Internship.js
@@ -0,0 +1,30 @@
+//Taijasi
+import "../SoftwareConsulting/Consulting.css"
+import CoreLanguage from '../../Pages/CoreLanguage';
+import WebPlatforms from '../../Pages/WebPlatforms';
+import WindowsPlatforms from '../../Pages/WindowPlatform';
+import CloudTechnologies from '../../Pages/CloudTechnologies';
+import Arti from '../../Pages/ArtificialIntelligence';
+import Certificates from '../../Pages/Certificates';
+import { Component } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ScrollCardsDetails from "../Cards/ScrollCards/InternshipScrollCard";
+
+class Internships extends Component {
+  render() {
+    return (
+     
+            <div className="App">
+                <header className="App-header">
+                    <h1>Internships and Trainings</h1>
+                  
+                </header>
+                
+                </div>
+
+                
+    );
+  }
+}
+export default Internships;
